bf5de6e9871e51b5317a33f7bc9b25575e872d8c
Mélissa BEUZE <melissa.beuze@gmail.com>
2020-05-07 15:50:46 +0400

    Edit: Optimized route.js file

--- src/Routes.js
+++ src/Routes.js
@@ -1,64 +1,36 @@
-import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import React from "react";
+import { Switch, Redirect } from "react-router-dom";
 
-import { RouteWithLayout } from './components';
-import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+import { RouteWithLayout } from "./components";
+import { Main as MainLayout, Minimal as MinimalLayout } from "./layouts";
 
-import {
-  Dashboard as DashboardView,
-  NotFound as NotFoundView
-} from './views';
+import { Dashboard as DashboardView, NotFound as NotFoundView } from "./views";
+
+const routes = [
+  { path: "/dashboard", component: DashboardView },
+  { path: "/contacts", component: DashboardView },
+  { path: "/organizations", component: DashboardView },
+  { path: "/categories", component: DashboardView },
+  { path: "/countries", component: DashboardView },
+  { path: "/cities", component: DashboardView },
+  { path: "/account", component: DashboardView },
+  { path: "/settings", component: DashboardView },
+  { path: "/not-found", component: NotFoundView, layout: MinimalLayout },
+];
 
 const Routes = () => {
   return (
     <Switch>
-      <Redirect
-        exact
-        from="/"
-        to="/dashboard"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/dashboard"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/contacts"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/organizations"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/categories"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/countries"
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/cities"
-      />
-      <RouteWithLayout
-        component={NotFoundView}
-        exact
-        layout={MinimalLayout}
-        path="/not-found"
-      />
+      <Redirect exact from="/" to="/dashboard" />
+      {routes.map((route, index) => (
+        <RouteWithLayout
+          key={index}
+          component={route.component}
+          exact
+          layout={route.layout || MainLayout}
+          path={route.path}
+        />
+      ))}
       <Redirect to="/not-found" />
     </Switch>
   );
